200dadd63f863a2cd5e82e1004bdda0ed9a4eed1
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-01-19 13:07:55 +0300

    update data.sql

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,25 +1,26 @@
-insert into dish (id, name, dish_type)
-values (1, 'Salaide fraîche', 'STARTER'),
-       (2, 'Poulet grillé', 'MAIN'),
-       (3, 'Riz aux légumes', 'MAIN'),
-       (4, 'Gâteau au chocolat ', 'DESSERT'),
-       (5, 'Salade de fruits', 'DESSERT');
+insert into dish (id, name, dish_type,price)
+values (1, 'Salaide fraîche', 'STARTER',3500.00),
+       (2, 'Poulet grillé', 'MAIN',12000.00),
+       (3, 'Riz aux légumes', 'MAIN',null),
+       (4, 'Gâteau au chocolat ', 'DESSERT',8000.00),
+       (5, 'Salade de fruits', 'DESSERT',null);
 
 
-insert into ingredient (id, name, category, price, id_dish)
-values (1, 'Laitue', 'VEGETABLE', 800.0, 1),
-       (2, 'Tomate', 'VEGETABLE', 600.0, 1),
-       (3, 'Poulet', 'ANIMAL', 4500.0, 2),
-       (4, 'Chocolat ', 'OTHER', 3000.0, 4),
-       (5, 'Beurre', 'DAIRY', 2500.0, 4);
+insert into ingredient (id, name, category, price)
+values (1, 'Laitue', 'VEGETABLE', 800.0),
+       (2, 'Tomate', 'VEGETABLE', 600.0),
+       (3, 'Poulet', 'ANIMAL', 4500.0),
+       (4, 'Chocolat ', 'OTHER', 3000.0),
+       (5, 'Beurre', 'DAIRY', 2500.0);
+
+
+insert into  DishIngredient (id,id_dish,id_ingredient,quantity_required, unit)
+values (1,1,1,0.20,'KG'),
+       (2,1,2,0.15,'KG'),
+       (3,2,3,1.00,'KG'),
+       (4,4,4,0.30,'KG'),
+       (5,4,5,0.20,'KG');
 
 
 
-update dish
-set price = 2000.0
-where id = 1;
-
-update dish
-set price = 6000.0
-where id = 2;
 
---
+++ src/main/resources/sql/new_schema.sql
@@ -0,0 +1,33 @@
+create type dish_type as enum ('STARTER', 'MAIN', 'DESSERT');
+
+create table dish
+(
+    id        serial primary key,
+    name      varchar(255),
+    dish_type dish_type,
+    price numeric(10,2)
+);
+
+create type ingredient_category as enum ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+create table ingredient
+(
+    id       serial primary key,
+    name     varchar(255),
+    price    numeric(10, 2),
+    category ingredient_category
+);
+
+create type unit_type as enum ('PCS','KG','L');
+
+create table  DishIngredient (
+    id serial primary key ,
+    id_dish int references dish(id),
+    id_ingredient int references ingredient(id),
+    quantity_required numeric(10, 2),
+    unit unit_type
+);
+
+
+alter table dish
+    add column if not exists price numeric(10, 2);
